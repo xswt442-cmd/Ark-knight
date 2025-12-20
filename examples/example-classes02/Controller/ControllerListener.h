566355dce7a2617b92136a1e7dd1fb028e91c026
PracticalIssue <xswt442@gmail.com>
2025-12-20 15:32:25 +0800

    [Chore]杂项文件无视

--- examples/example-classes02/Controller/ControllerListener.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "cocos2d.h"
-using namespace cocos2d;
-class ControllerListener
-{
-public:
-	virtual void setTagPosition(const int x, const int y) = 0;
-	virtual Point getTagPosition()const = 0;
-};
