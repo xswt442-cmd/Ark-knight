e297966e09697e1f00b5ffa2b4fe78c77508addb
PracticalIssue <xswt442@gmail.com>
2025-12-24 20:53:50 +0800

    [Fix]修改设置菜单显示的问题

--- Ark-knight/Classes/UI/SettingsLayer.cpp
+++ Ark-knight/Classes/UI/SettingsLayer.cpp
@@ -16,21 +16,24 @@ void SettingsLayer::createUI()
 {
     // 半透明遮罩
     auto mask = LayerColor::create(Color4B(0, 0, 0, 180));
+    mask->setGlobalZOrder(Constants::ZOrder::UI_GLOBAL + 10);
     this->addChild(mask);
     
     // 设置标题
     auto settingsTitle = Label::createWithTTF(u8"设置", "fonts/msyh.ttf", 56);
     settingsTitle->setPosition(Vec2(SCREEN_CENTER.x, SCREEN_CENTER.y + 200));
     settingsTitle->setTextColor(Color4B::WHITE);
+    settingsTitle->setGlobalZOrder(Constants::ZOrder::UI_GLOBAL + 11);
     this->addChild(settingsTitle);
     
-    // 分辨率标签
+    // 分辨率标签 - 左移到屏幕左侧1/4位置
     auto resLabel = Label::createWithTTF(u8"分辨率：", "fonts/msyh.ttf", 32);
-    resLabel->setPosition(Vec2(SCREEN_CENTER.x - 200, SCREEN_CENTER.y + 100));
+    resLabel->setPosition(Vec2(SCREEN_CENTER.x * 0.5f, SCREEN_CENTER.y + 100));
     resLabel->setTextColor(Color4B::WHITE);
+    resLabel->setGlobalZOrder(Constants::ZOrder::UI_GLOBAL + 11);
     this->addChild(resLabel);
     
-    // 分辨率选项按钮
+    // 分辨率选项按钮 - 调整起始位置，左移整体
     const char* resolutions[] = {u8"800x600", u8"1280x720", u8"1920x1080"};
     int resWidth[] = {800, 1280, 1920};
     int resHeight[] = {600, 720, 1080};
@@ -41,7 +44,9 @@ void SettingsLayer::createUI()
         resButton->setTitleText(resolutions[i]);
         resButton->setTitleFontName("fonts/msyh.ttf");
         resButton->setTitleFontSize(28);
-        resButton->setPosition(Vec2(SCREEN_CENTER.x + 80 + i * 200, SCREEN_CENTER.y + 100));
+        // 从屏幕中心往右偏移，间距180
+        resButton->setPosition(Vec2(SCREEN_CENTER.x + i * 180 - 120, SCREEN_CENTER.y + 100));
+        resButton->setGlobalZOrder(Constants::ZOrder::UI_GLOBAL + 11);
         int width = resWidth[i];
         int height = resHeight[i];
         resButton->addClickEventListener([width, height](Ref* sender) {
@@ -60,13 +65,11 @@ void SettingsLayer::createUI()
     backButton->setTitleFontName("fonts/msyh.ttf");
     backButton->setTitleFontSize(32);
     backButton->setPosition(Vec2(SCREEN_CENTER.x, SCREEN_CENTER.y - 100));
+    backButton->setGlobalZOrder(Constants::ZOrder::UI_GLOBAL + 11);
     backButton->addClickEventListener([this](Ref* sender) {
         close();
     });
     this->addChild(backButton);
-    
-    // 设置全局Z顺序
-    this->setGlobalZOrder(Constants::ZOrder::UI_GLOBAL + 10);
 }
 
 void SettingsLayer::close()
